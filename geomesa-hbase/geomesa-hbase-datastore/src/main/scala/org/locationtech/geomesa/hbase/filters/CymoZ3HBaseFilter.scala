eab9e39a433dd1193d40949c091e739811aeb8ad
LAccordeur <610641902@qq.com>
2021-07-21 19:35:50 +0800

    server side filter

---
+++ geomesa-hbase/geomesa-hbase-datastore/src/main/scala/org/locationtech/geomesa/hbase/filters/CymoZ3HBaseFilter.scala
@@ -0,0 +1,10 @@
+package org.locationtech.geomesa.hbase.filters
+
+/**
+  * @Description
+  * @Date 2021/7/21 14:54
+  * @Created by X1 Carbon
+  */
+class CymoZ3HBaseFilter {
+
+}
---
+++ geomesa-index-api/src/main/scala/org/locationtech/geomesa/index/filters/CymoZ3Filter.scala
@@ -0,0 +1,10 @@
+package org.locationtech.geomesa.index.filters
+
+/**
+  * @Description
+  * @Date 2021/7/21 11:59
+  * @Created by X1 Carbon
+  */
+class CymoZ3Filter {
+
+}
